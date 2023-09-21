685a7565af4928bd2b6bd736f299d732a18fdac9
Se Hwan <realsehwan@gmail.com>
2023-09-21 20:07:00 +0900

    feat: add 2 cmds
    1.  텍스트 변화에 따른 글자수 변화 기록
    2. workspace state 초기화

--- extension.js
+++ extension.js
@@ -1,46 +1,66 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+function getKeyFromCurrentTimeStamp() {
+    const now = new Date()
+    const minutes = now.getMinutes()
+    if (minutes < 30) {
+        now.setMinutes(0)
+    }
+    else {
+        now.setMinutes(30)
+    }
+    
+    return now.toISOString().substring(0, 16) + 'Z'    
+}
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "cpt" is now active!')
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let hello = vscode.commands.registerCommand(
-        'cpt.helloWorld',
-        function () {
-            // The code you place here will be executed every time your command is executed
+    let veTest = vscode.commands.registerCommand(
+        'cpt.veTest',
+        () => {
+            vscode.workspace.onDidChangeTextDocument((event) => {
+                if (!event.document.isUntitled) {
+                    let addNum = 0
+                    let deleteNum = 0
+    
+                    for (let i of event.contentChanges) {
+                        addNum += i.text.length
+                        deleteNum += i.rangeLength
+                    }
 
-            // Display a message box to the user
-            vscode.window.showInformationMessage('Hello VSCode!')
+                    if (addNum !== 0 || deleteNum !== 0) {
+                        const key = getKeyFromCurrentTimeStamp()
+                        let changeObject = context.workspaceState.get(key)
+                        if (changeObject === undefined) {
+                           changeObject = {addTotal: 0, deleteTotal: 0} 
+                        }
+    
+                        context.workspaceState.update(key, {addTotal: changeObject.addTotal+addNum, deleteTotal: changeObject.deleteTotal+deleteNum})
+                        console.log(context.workspaceState.get(key))
+                    }
+                }
+            })
         }
     )
 
-    let clock = vscode.commands.registerCommand(
-        'cpt.clock',
-        function () {
-            // The code you place here will be executed every time your command is executed
-			let cur = new Date()
-            // Display a message box to the user
-            vscode.window.showErrorMessage(cur.toISOString())
+    let resetState = vscode.commands.registerCommand(
+        'cpt.resetState',
+        () => {
+            context.workspaceState.keys().forEach((key) => {
+                context.workspaceState.update(key, undefined)
+            })
+            console.log('workspaceState was reset!')
         }
     )
 
-    context.subscriptions.push(hello)
-	context.subscriptions.push(clock)
+    context.subscriptions.push(veTest)
+    context.subscriptions.push(resetState)
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
